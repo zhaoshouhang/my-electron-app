751f155eef06c5eb516a51225df3a8fff5c0ba49
zhaoshouhang <zhaoshouhang9295@icloud.com>
2024-07-03 22:19:21 +0800

    完成版本号的添加

---
+++ preload.js
@@ -0,0 +1,11 @@
+// 这一步 我目前的理解就是node 的后端程序
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
+  }
+});
