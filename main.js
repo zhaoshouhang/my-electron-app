99227512c9fb3045d9ca728963a29b480f0c10e2
zhaoshouhang <zhaoshouhang9295@icloud.com>
2024-07-03 22:23:33 +0800

    官方的

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
+// main.js
+
+// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require("electron");
-const path = require("path");
-// 这个函数用于创建窗口对象并加载html
+const path = require("node:path");
+
 const createWindow = () => {
-  const win = new BrowserWindow({
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    //这里提供预加载的js
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  win.loadFile("index.html");
+  // 加载 index.html
+  mainWindow.loadFile("index.html");
+
+  // 打开开发工具
+  // mainWindow.webContents.openDevTools()
 };
-// 这个主要用于当程序启动完成后,创建窗口对象并渲染
+
+// 这段程序将会在 Electron 结束初始化
+// 和创建浏览器窗口的时候调用
+// 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(() => {
   createWindow();
+
+  app.on("activate", () => {
+    // 在 macOS 系统内, 如果没有已开启的应用窗口
+    // 点击托盘图标时通常会重新创建一个新窗口
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-//监听 当激活的时候 判断一下是否要继续创建窗口
-//这个是macos下的逻辑
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-//监听
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
+// 对应用程序和它们的菜单栏来说应该时刻保持激活状态,
+// 直到用户使用 Cmd + Q 明确退出
 app.on("window-all-closed", () => {
-  //macos下的逻辑
   if (process.platform !== "darwin") app.quit();
 });
+
+// 在当前文件中你可以引入所有的主进程代码
+// 也可以拆分成几个文件，然后用 require 导入。
--- preload.js
+++ preload.js
@@ -1,4 +1,7 @@
-// 这一步 我目前的理解就是node 的后端程序
+// preload.js
+
+// 所有的 Node.js API接口 都可以在 preload 进程中被调用.
+// 它拥有与Chrome扩展一样的沙盒。
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
